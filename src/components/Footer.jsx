37814fd3bcb6afcae9e14ecf25cd86f603fd7709
AhmedSaeed216 <ahmed.saeed6420@gmail.com>
2024-09-28 23:06:23 +0500

    update the landing page and other ui

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,38 @@
+// import React from 'react';
+// import './Footer.css';
+
+// export const Footer = () => {
+//   return (
+//     <footer>
+//       <div className="footer-content">
+//         <div className="logo-footer">CALL IT STUDIO</div>
+//         <div className="quick-links">
+//           <h4>Quick Links</h4>
+//           <ul>
+//             <li>Privacy Policy</li>
+//             <li>Terms & Conditions</li>
+//           </ul>
+//         </div>
+//         <div className="contact">
+//           <p>Subscribe to our newsletter</p>
+//           <input type="email" placeholder="Enter email address" />
+//           <p>Ph: 03xx-xxxxxxx</p>
+//           <p>Email: xyz@gmail.com</p>
+//           <div className="social-icons">
+//             <i className="fab fa-linkedin"></i>
+//             <i className="fab fa-instagram"></i>
+//             <i className="fab fa-facebook"></i>
+//             <i className="fab fa-github"></i>
+//           </div>
+//         </div>
+//       </div>
+//       <br />
+//     </footer>
+//   );
+// };
+
+
+
 import React from 'react';
 import './Footer.css';
 
@@ -26,8 +61,11 @@ export const Footer = () => {
           </div>
         </div>
       </div>
+      <br className='line'/>
+      {/* Rights Reserved Section */}
+      <div className="footer-bottom">
+        <p>&copy; 2024 CALL IT STUDIO. All rights reserved.</p>
+      </div>
     </footer>
   );
 };
-
-
