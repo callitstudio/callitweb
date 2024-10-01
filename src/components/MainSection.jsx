14b12849c34a1bc4f49cb46c7f6862d7ff826883
AhmedSaeed216 <ahmed.saeed6420@gmail.com>
2024-10-01 20:08:18 +0500

    re edit main section

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import './MainSection.css';
-import mainImg from "../assets/Media/mainSectionImg.jpg"
+// import mainImg from "../assets/Media/mainSectionImg.jpg"
 import video from "../assets/Media/animiVideo.mp4"
 export const MainSection = () => {
   return (
@@ -11,11 +11,11 @@ export const MainSection = () => {
           <div className="col-md-6 col-sm-12">
             <h1>CALL IT STUDIO</h1>
             <p>We’re a passionate team of creative and tech-savvy individuals who love working with digital solutions. With experts in media, IT, and marketing, each of us brings unique skills to the table. Together, we aim to combine creativity and technology to produce amazing results.</p>
+          </div>
             <button className="cta-btn">Learn More</button>
-          </div>
-          <div className="col-md-6 col-sm-12 gif-section">
-            <img   src={gif} alt="Creative Work GIF" className="responsive-gif" />
-          </div>
+          {/* <div className="col-md-6 col-sm-12 gif-section">
+            <img   src={""} alt="Creative Work GIF" className="responsive-gif" />
+          </div> */}
         </section>
 
         <section className='right '>
