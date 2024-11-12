4d34d5426a009a078c837eb3e98070289c4e552e
AhmedSaeed216 <ahmed.saeed6420@gmail.com>
2024-11-12 09:28:35 +0500

    images added in services + adjust in moile view

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -7,6 +7,13 @@ import itgif from '../assets/images/it.gif';
 import studiogif from '../assets/images/studio.gif';
 import eventgif from '../assets/images/event.gif';
 import marketinggif from '../assets/images/marketing.gif';
+// import content from "../assets/images/content.gif"
+// import media from "../assets/images/media.gif"
+// import event from "../assets/images/event.gif"
+// import it from "../assets/images/it.gif"
+// import marketing from "../assets/images/marketing.gif"
+// import studio from "../assets/images/studio.gif"
+// import event from "../assets/images/event.gif"
 
 
 export const Services = () => {
@@ -31,38 +38,58 @@ export const Services = () => {
         <div className={`service-item ${loaded ? 'loaded' : ''}`} data-number="1" onClick={() => handleNavigate('/media')}>
           <h3>Media</h3>
           <p>We deliver creative media solutions that elevate your brand and captivate audience...</p>
-          <img src={mediagif} alt="Media" className="service-image" />
-        </div>
+        {/* <div className="service-image" > */}
+          <img src={mediagif} alt="Media" />
+        {/* </div> */}
+        </div >
+
+
 
         <div className={`service-item ${loaded ? 'loaded' : ''}`} data-number="2" onClick={() => handleNavigate('/ContentWriting')}>
           <h3>Content Writing</h3>
           <p>We craft narratives that inspire action...</p>
-          <img src={contentgif} alt="Content Writing" className="service-image" />
+        {/* <div className='service-item'> */}
+          <img src={contentgif} alt="Content Writing" />
+        {/* </div> */}
         </div>
+
 
         <div className={`service-item ${loaded ? 'loaded' : ''}`} data-number="3" onClick={() => handleNavigate('/marketing')}>
           <h3>Marketing</h3>
           <p>We craft innovative marketing strategies...</p>
+        {/* <div className="service-image"> */}
           <img src={marketinggif} alt="Marketing" className="service-image" />
+        {/* </div> */}
         </div>
+
 
         <div className={`service-item ${loaded ? 'loaded' : ''}`} data-number="4" onClick={() => handleNavigate('/it')}>
           <h3>IT</h3>
           <p>We develop user-friendly websites...</p>
+          {/* <div className="service-image"> */}
           <img src={itgif} alt="IT" className="service-image" />
+          {/* </div> */}
         </div>
+          
 
         <div className={`service-item ${loaded ? 'loaded' : ''}`} data-number="5" onClick={() => handleNavigate('/studio')}>
           <h3>Studio</h3>
           <p>We can manage your social handle efficiently...</p>
+        {/* <div className="service-image"> */}
           <img src={studiogif} alt="Studio" className="service-image" />
+        {/* </div> */}
         </div>
+
 
         <div className={`service-item ${loaded ? 'loaded' : ''}`} data-number="6" onClick={() => handleNavigate('/events')}>
           <h3>Event Management</h3>
           <p>Get your important events managed by us...</p>
-          <img src={eventgif} alt="Event Management" className="service-image" />
+      {/* <div className="service-image"> */}
+        <img src={eventgif} alt="Event Management" className="service-image" />
+        {/* </div>   */}
         </div>
+
+      
       </section>
     </section>
   );
