1ba3475ee7cbf2978f5a29fc1537e0cea1d2fe3a
unknown <hr3261331@gmail.com>
2025-05-20 13:34:53 +0500

    new chnages

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,12 +4,11 @@ import CalendlyScheduler from '@/components/CalendlyScheduler';
 export default function Contact() {
   return (
     <div className="container mx-auto px-5 sm:px-8 md:px-10 lg:px-12 xl:px-16 2xl:px-20 py-12 xl:max-w-7xl 2xl:max-w-screen-2xl">
-      <h1 className="text-3xl font-bold mb-8">Contact Us</h1>
+      {/* <h1 className="text-3xl font-bold mb-8 text-center">Contact Us</h1>
 
-      {/* Contact Form Section */}
       <div className="max-w-md mx-auto mb-16">
         <ContactForm />
-      </div>
+      </div> */}
 
       {/* Appointment Scheduler Section */}
       <div className="mt-16 mb-10">
